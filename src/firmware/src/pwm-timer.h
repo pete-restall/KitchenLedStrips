4edcd9e9654d5f7603b5c6fed74d258663042bfa
pete-restall <pete@restall.net>
2023-08-14 20:59:24 +0100

    Added a skeleton PWM timer module.  Added basic pin initialisation.

---
+++ src/firmware/src/pwm-timer.h
@@ -0,0 +1,8 @@
+#ifndef __KITCHEN_LEDS_PWMTIMER_H
+#define __KITCHEN_LEDS_PWMTIMER_H
+
+extern void pwmTimerInitialise(void);
+extern void pwmTimerEnable(void);
+extern void pwmTimerDisable(void);
+
+#endif
