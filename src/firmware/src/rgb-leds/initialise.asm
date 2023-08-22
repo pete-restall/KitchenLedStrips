896a4731ba0ae26aa7e3522ed20d8f246d99c587
pete-restall <pete@restall.net>
2023-08-22 20:08:11 +0100

    Added initialisation code for the RGB LEDs module.

--- src/firmware/src/rgb-leds/initialise.asm
+++ src/firmware/src/rgb-leds/initialise.asm
@@ -1,9 +1,20 @@
+	#include "rgb-leds.inc"
+	#include "sm16703p-modulator.inc"
+
 	radix decimal
+
+	extern isrTxBufferReset
 
 .rgbleds code
 	global rgbLedsInitialise
 
 rgbLedsInitialise:
+	pagesel isrTxBufferReset
+	call isrTxBufferReset
+
+	pagesel sm16703pModulatorInitialise
+	call sm16703pModulatorInitialise
+
 	return
 
 	end
