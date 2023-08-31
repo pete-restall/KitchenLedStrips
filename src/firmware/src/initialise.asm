57ed7a8a5e5ff768cb975e33f8ac8109bfc3ac7b
pete-restall <pete@restall.net>
2023-08-31 18:20:00 +0100

    Added some TODOs for future requirements.

--- src/firmware/src/initialise.asm
+++ src/firmware/src/initialise.asm
@@ -8,6 +8,13 @@
 	global initialise
 
 initialise:
+	; TODO: Different reset behaviours are required.
+	;   If POR then turn on full (with default settings).
+	;   If BOR then turn on full (perhaps with previous settings, if not wiped from RAM ?)
+	;   If WDT then same as BOR or perhaps just turn off all LEDs ?
+
+	; TODO: Various settings not yet considered, such as PCON for enabling stack over/underflow resets
+
 	pagesel powerManagementInitialise
 	call powerManagementInitialise
 
