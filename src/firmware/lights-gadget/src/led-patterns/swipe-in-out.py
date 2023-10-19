6e67fd93d0c1ff963a441fc0112c0b13bd556975
pete-restall <pete@restall.net>
2023-10-19 14:37:11 +0100

    A simpler algorithm that takes into account how the pixels are written by the firmware.  Again, not perfect, but good enough.

--- src/firmware/lights-gadget/src/led-patterns/swipe-in-out.py
+++ src/firmware/lights-gadget/src/led-patterns/swipe-in-out.py
@@ -1,10 +1,10 @@
-number_of_pixels = 63
+number_of_pixels = 64
 centre_pixel_indices = [(number_of_pixels - 1) >> 1, number_of_pixels >> 1]
 pixels = [0] * number_of_pixels
 
 frame_period = 1 / 50
 step_delay = 1
-step_size_pixels = 3
+step_size_pixels = 1
 number_of_shades = 4
 
 if step_size_pixels > centre_pixel_indices[0]:
@@ -22,12 +22,11 @@ def do_swipe(shade_increment, pixel_done_index, pixel_done_value):
 	time = 0
 	frame_index = 0
 	delay_index = 0
-	odd_number_of_pixels_adjustment = 0 if step_size_pixels == 1 else (number_of_pixels & 1)
 
 	if shade_increment > 0:
-		left_index = centre_pixel_indices[0] + step_size_pixels // 2 - odd_number_of_pixels_adjustment
-		right_index = centre_pixel_indices[1] - step_size_pixels // 2 + odd_number_of_pixels_adjustment
-	else:
+		left_index = centre_pixel_indices[0]
+		right_index = centre_pixel_indices[1]
+	elif shade_increment < 0:
 		left_index = step_size_pixels - 1
 		right_index = number_of_pixels - step_size_pixels
 
@@ -37,36 +36,31 @@ def do_swipe(shade_increment, pixel_done_index, pixel_done_value):
 			if pixels[pixel_done_index] == pixel_done_value:
 				break
 
-			step_size_index = 0
-			for i in range(0, step_size_pixels * number_of_shades):
-				if (left_index - i < right_index) and (left_index - i >= 0):
-					pixels[left_index - i] += shade_increment
+			if shade_increment > 0:
+				for i in range(0, number_of_pixels):
+					current_pixel = pixels[i]
+					if i >= left_index and i <= right_index:
+						if (i < left_index + step_size_pixels) or (i > right_index - step_size_pixels):
+							pixels[i] = 1
+						elif current_pixel != number_of_shades:
+							pixels[i] = current_pixel + 1
+					else:
+						pixels[i] = current_pixel
 
-				if right_index + i <= number_of_pixels - 1:
-					pixels[right_index + i] += shade_increment
-				else:
-					break
-
-				if shade_increment > 0 and (pixels[right_index + i] == 1) and (step_size_index == step_size_pixels - 1):
-					break
-
-				if shade_increment < 0 and (right_index + i + 1 < number_of_pixels) and (pixels[right_index + i + 1] == 0):
-					break
-
-				step_size_index += 1
-				if step_size_index >= step_size_pixels:
-					step_size_index = 0
-
-			if shade_increment > 0 and pixels[right_index] == number_of_shades:
 				left_index -= step_size_pixels
 				right_index += step_size_pixels
 
-			if shade_increment < 0 and pixels[right_index] == number_of_shades - 1:
+			elif shade_increment < 0:
+				for i in range(0, number_of_pixels):
+					current_pixel = pixels[i]
+					if i <= left_index or i >= right_index:
+						if current_pixel != 0:
+							pixels[i] = current_pixel - 1
+					else:
+						pixels[i] = number_of_shades
+
 				left_index += step_size_pixels
 				right_index -= step_size_pixels
-				if left_index > right_index:
-					left_index = centre_pixel_indices[0]
-					right_index = centre_pixel_indices[1]
 
 		delay_index += 1
 		if delay_index > step_delay:
